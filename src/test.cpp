b653a47655f4ca169062ac480b9d18de99f913f6
KenshiroHosaka <s1226093pl@s.chibakoudai.jp>
2017-02-03 05:04:09 +0900

    [Add] TravisCI test

---
+++ src/test.cpp
@@ -0,0 +1,12 @@
+/*
+ * test.cpp
+ * 作成日；2017/02/03
+ * 作成者：ken
+ * TravisCIの使い方確認用プログラム
+ *
+ */
+#include <iostream>
+int main(){
+    std::cout << "Hello TravisCI" << std::endl;
+    return 0;
+}
